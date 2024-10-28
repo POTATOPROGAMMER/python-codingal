c59e1b96387bfbc44d7b2e9cd3a034afccc04650
POTATOPROGAMMER <codercode1010@gmail.com>
2024-10-28 17:57:34 +0400

    commit message

---
+++ L2/activity 1.py
@@ -0,0 +1,6 @@
+print ("hello world")
+print (4)
+#to print text in new line
+print ("hello\n python\n")
+#to print a combination of text and numbers
+print ("hello ",4)
---
+++ L2/activity 2.py
@@ -0,0 +1,6 @@
+x=5
+print(x)
+y="thahira"
+print (y)
+z=input ("enter your name ")
+print (z)
