2d1c55c10f2d26bbfd879c7c0ac8dc41b6a43135
POTATOPROGAMMER <codercode1010@gmail.com>
2024-10-31 17:46:10 +0400

    commit

---
+++ L3/activity 1.py
@@ -0,0 +1,16 @@
+name="zayed"
+age=11
+weight=34.5
+student=True
+
+
+
+
+
+print ("my name is",name)
+print ("my age is",age)
+age=9
+print ("my age is",age)
+print ("data type of age is",type(age))
+print ("data type of weight is",type(weight))
+print ("data type of student is",type(student))
